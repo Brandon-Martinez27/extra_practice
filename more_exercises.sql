4060d74f31f5f1ac1fe9f93be0270d21f23c42e4
“Brandon <Brandon.joseph.martinez3@gmail.com>
2021-01-22 10:04:26 -0600

    started new section on more sql exercises

--- more_exercises.sql
+++ more_exercises.sql
@@ -41,6 +41,8 @@ JOIN (#average salary per dept
 ORDER BY mgr_salary_difference DESC
 ;
 
+------------------------------------------------------------------------------
+
 -- World Database
 
 -- Use the world database for the questions below.
@@ -248,3 +250,61 @@ ORDER BY AVG(LifeExpectancy);
 +---------------------------+-----------------+
 25 rows in set (0.00 sec)
 */
+
+
+/*
+Bonus
+
+- Find all the countries whose local name is different from the official name
+- How many countries have a life expectancy less than x?
+- What state is city x located in?
+- What region of the world is city x located in?
+- What country (use the human readable name) city x located in?
+- What is the life expectancy in city x?
+*/
+----------------------------------------------------------------------------------
+
+-- Sakila Database
+
+USE sakila;
+
+-- 1. Display the first and last names in all lowercase of all the actors.
+
+SELECT LOWER(CONCAT(first_name, ' ', last_name)) as 'name'
+FROM actor;
+
+/*
++---------------------+
+| name                |
++---------------------+
+| penelope guiness    |
++---------------------+
+| nick wahlberg       |
++---------------------+
+| ed chase            |
++---------------------+
+| jennifer davis      |
++---------------------+
+| johnny lollobrigida |
++---------------------+
+| bette nicholson     |
++---------------------+
+| grace mostel        |
++---------------------+
+| matthew johansson   |
++---------------------+
+| joe swank           |
++---------------------+
+| christian gable     |
++---------------------+
+| zero cage           |
++---------------------+
+| karl berry          |
++---------------------+
+| uma wood            |
++---------------------+
+| vivien bergen       |
++---------------------+
+200 rows in set
+*/
+

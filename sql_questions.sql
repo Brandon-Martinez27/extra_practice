426804458572ec2467add57e4f86d94a472fba90
“Brandon <Brandon.joseph.martinez3@gmail.com>
2021-04-11 12:28:12 -0500

    answered 2 questions with examples and explanations

--- sql_questions.sql
+++ sql_questions.sql
@@ -15,9 +15,36 @@ Example: query that uses the key word and result.
 Answer: A WHERE clause is used to filter records from a result. 
 The filter occurs before any groupings are made.
 A HAVING clause is used to filter values from a group
- 
+*/
+-- Example of WHERE:
+SELECT COUNT(salary)
+FROM salaries
+WHERE salary > 60000; -- 1,495,933 rows affected
+-- vs HAVING:
+SELECT emp_no, AVG(salary) as 'avg_salary'
+FROM salaries
+GROUP BY emp_no
+HAVING avg_salary > 60000; -- 134,369 rows affected
+/* explanation: WHERE will filter out based on indivdual rows,
+HAVING will filter based on group/aggregate results */
+
 
 -- 2. When are COUNT(*) and COUNT DISTINCT equal?
+/* 
+Answer: They are both equal when referring to a primary key
+where each value is in fact distinct/unique from any other in
+that row.
+*/
+-- Example of :
+SELECT COUNT(emp_no)
+FROM salaries; -- 284,4047 result
+-- vs :
+SELECT COUNT(DISTINCT(emp_no))
+FROM salaries; -- 300,024
+/* explanation: in this example there may be more than one salary
+for each employee as they get promoted (and hopefull less likely 
+demoted) within the company. In the table where employee id is the
+primary key though (`employees`), both results are the same. */
 
 -- 3. Can left and inner join return the same results?
 

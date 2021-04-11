c2de9a0a6c47f9fb2e28c13a6309e0f9a1faf895
“Brandon <Brandon.joseph.martinez3@gmail.com>
2021-04-11 11:59:36 -0500

    started SQL practice questions with examples

---
+++ sql_questions.sql
@@ -0,0 +1,36 @@
+/* 
+Questions proposed by Eric Weber on Linkedin
+
+I thought it would be fun and interesting to answer some
+of these questions using an employee database
+
+Answer: the answer to the question. May include syntax and definitions 
+of key words like functions, clauses, etc.
+
+Example: query that uses the key word and result.
+*/
+
+-- 1. What is the difference between HAVING and WHERE?
+/* 
+Answer: A WHERE clause is used to filter records from a result. 
+The filter occurs before any groupings are made.
+A HAVING clause is used to filter values from a group
+ 
+
+-- 2. When are COUNT(*) and COUNT DISTINCT equal?
+
+-- 3. Can left and inner join return the same results?
+
+-- 4. What is the difference between RANK and DENSE RANK?
+
+-- 5. Can UNION and UNION ALL return the same results?
+
+-- 6. What are the best scenarios to use a self join?
+
+-- 7. How can you use SQL to dedupe rows?
+
+-- 8. In what scenarios is the LAG function useful?
+
+-- 9. Use a window function to calculate a running total.
+
+-- 10. Why would you use GROUP BY ROLLUP?
